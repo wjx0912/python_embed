91c57f87ded9180c7cdb83f0e34b9e29975abbed
wyt <80101277@qq.com>
2024-08-20 22:30:18 +0800

    none

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ import cv2
 import torch
 import ultralytics
 
-print(cv2.__version__)
-print(torch.__version__)
-print(ultralytics.__version__)
+print("cv2.__version__: ", cv2.__version__)
+print("torch.__version__: ", torch.__version__)
+print("ultralytics.__version__: ", ultralytics.__version__)
 print("Hello, World!")
